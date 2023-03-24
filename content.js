f9ce1a70e3be35fdc34355607a172c14d5d8bdb8
ryan-bello <ryanbello48@gmail.com>
2023-03-23 22:07:35 -0700

    Added Ditto Icon, added link to vods.co in popup, disabled content.js on main page and other pages, refactored code such that popup waits till it receives a response from content.js, added comments

--- content.js
+++ content.js
@@ -1,45 +1,36 @@
-var filter_on = false
-var old_columns = null
+var old_columns = null //Used to store old data after we apply the filter, so we can restore it later
 
 if (document.readyState !== 'loading') { // If the document has already loaded, run initializing function. Otherwise, wait.
-    start();
+    start()
 } else {
     document.addEventListener('DOMContentLoaded', function () {
-        start();
-    });
+        start()
+    })
 }
 
-function start () {
-	chrome.storage.local.get(["filter_on"]).then((result) => {
-		if (result.filter_on !== null) {
-			console.log("content.js -- filter_on_value read from local storage is " + result.filter_on);
-			filter_on = result.filter_on
-		} else {
-			console.log("content.js -- filter_on is undefined in local storage. Defaulting to false")
-		}
-
-		console.log("filter_on = ", filter_on) //If filter is set to on in local, initialize with filter on
-		if (filter_on) {
-			console.log("enabling filter")
+function start() {
+	chrome.storage.local.get(["is_filter_on"]) //Check local storage
+	.then((result) => {
+		if (result.is_filter_on !== null && result.is_filter_on === true) { //enable the filter if storage says it is
 			enable_filter()
-		}
+		} 
 
-		chrome.runtime.onMessage.addListener( //only add the listener after the local storage has been read. 
+		chrome.runtime.onMessage.addListener( //Add a listener for the message sent by the button from popup.js
 			function(request, sender, sendResponse) {
-				if (filter_on) {
-					disable_filter()
-					filter_on = false;
-				} else {
+				if (request.action === "enable_filter") {
 					enable_filter()
-					filter_on = true
+				} else if (request.action === "disable_filter") {
+					disable_filter()
+				} else {
+					console.log("Warning: invalid message sent to content.js")
+					sendResponse({"successful": false})
 				}
-				sendResponse(true) //Send a resposne indicating if the filter is on or not
+				sendResponse({"successful": true}) //Send a response indicating if the filter is on or not
 			}
 		)
-	});
+	})
 }
 
-//var old_views_tables
 function disable_filter() { //restores the old columns to the document
 	var columns = document.getElementById("columns")
 	columns.innerHTML = old_columns.innerHTML
@@ -47,39 +38,34 @@ function disable_filter() { //restores the old columns to the document
 }
 
 function enable_filter() {
-	//Creating Elements
-
-	//Store the existing "columns" div, in case they want to turn the filter off
+	//Store the existing "columns" div in case the user later disables the filter
 	var columns = document.getElementById("columns")
 	old_columns = columns.cloneNode(true)
 
-	//Get all views-table (1 per date/tournament)
+	//Get all views-table (There is one per tournament)
 	var views_tables = document.getElementsByClassName("views-table")
-
-
-	for (let i = 0; i < views_tables.length; i++) { 
+	
+	for (let i = 0; i < views_tables.length; i++) { //Iterate through views_tables
 		var view_tables_element = views_tables[i]
-		var tbody = view_tables_element.getElementsByTagName('tbody') //Get tbody which is a list of matches
-		if (tbody.length !== 0) {
-			var match_list_tr = tbody[0].getElementsByTagName('tr')
+		var tbody = view_tables_element.getElementsByTagName('tbody') //Get tbody's (which is a list of matches)
+		if (tbody.length !== 0) { //confirm it is not an empty list
+			var match_list_tr = tbody[0].getElementsByTagName('tr')	//First tbody will contain the data
 
-			var isEven = false
+			var isEven = false //Used for replicating existing styling
 			//Iterate through each match, and  remove any match that is not a ditto.
 			for (let j = 0; j < match_list_tr.length; j++) {
 				var isDitto = false
-				//Get td elements
+				//Get td elements (contain all match details)
 				var match_list_td_list = match_list_tr[j].getElementsByTagName('td')
-				//Check for and grab the 2nd element (this should contain the match details)
+				//Check for and grab the 2nd element (this should contain the character icons and player names)
 				
 				if (match_list_td_list.length > 1) {
 					var match_details_td = match_list_td_list[1]
 					//Get a list of icons
-					var img_list = match_details_td.getElementsByTagName('img')
+					var img_list = match_details_td.getElementsByTagName('img') //Get list of images (character icons)
 					var img_src_set = new Set()
-					//console.log(img_list)
-					for (let k = 0; k < img_list.length; k++) {
+					for (let k = 0; k < img_list.length; k++) {	//Check if there is any duplicate icons. If there are, it indicates two players played the same character (likely a Ditto)
 						var img_src = img_list[k].getAttribute('src')
-						//console.log('img_src = ', img_src)
 						if (img_src_set.has(img_src)) { // A duplicate icon has been found
 							isDitto = true
 							break
--- popup.js
+++ popup.js
@@ -1,73 +1,106 @@
-var filter_on = false
+var is_filter_on = false 
 
 if (document.readyState !== 'loading') { // If the document has already loaded, run initializing function. Otherwise, wait.
-    start();
+    start()
 } else {
     document.addEventListener('DOMContentLoaded', function () {
-        start();
-    });
+        start()
+    })
 }
 
 function start() {
 
-	chrome.storage.local.get(["filter_on"]).then((result) => {
-		if (result.filter_on !== null) {
-			console.log("popup.js -- filter_on_value read from local storage is " + result.filter_on);
-			filter_on = result.filter_on
-		} else {
-			console.log("popup.js -- filter_on is undefined in local storage. Defaulting to false")
+	chrome.storage.local.get(["is_filter_on"]).then((result) => { //Read from local storage if the filter is on
+		if (result.is_filter_on !== null) {
+			is_filter_on = result.is_filter_on
+		} 
+
+	 	var filter_button = document.getElementById('filter_button') //Get the HTML Element for the button
+
+	 	if (is_filter_on) { //Button defaults to off when loaded in, so turn it on if it is
+			setButtonOn(filter_button) 
 		}
 
-		console.log("filter_on = ", filter_on)
-	 	var filter_button = document.getElementById('filter_button');
-
-	 	if (filter_on) {
-			setButtonOn(filter_button)
-		} else {
-			setButtonOff(filter_button)
-		}
-
-	  	filter_button.addEventListener('click', function() {
-	  		console.log("button clicked")
-	  		chrome.tabs.query({active: true, currentWindow: true}, function(tabs){
-	    		chrome.tabs.sendMessage(tabs[0].id, {greeting: "hello"})
-	    		.then(response => {
-	    			console.log("response = ", response)
-	    		})
-	    		.catch(error => {
-	    			console.error(error)
-	    		})
-			})
-			/*.then(response => { 
-				console.log("response = ", response)
-			})*/
-			//Alternate button text
-			//TODO: This should ideally  be done based on response (and the button should freeze and wait)
-			if (filter_on) {
-				setButtonOff(filter_button)
-				filter_on = false //set filter in memory
-				chrome.storage.local.set({ filter_on: filter_on }).then(() => { //Set filter_on in local storage (persistent)
-				  console.log("Filter on is set to " + filter_on);
-				});
+		//Add button listener
+	  	filter_button.addEventListener('click', function(){
+			if (is_filter_on) {
+				disable_filter(filter_button)
 			} else {
-				setButtonOn(filter_button)
-				filter_on = true 
-				chrome.storage.local.set({ filter_on: filter_on }).then(() => {
-				  console.log("Filter on is set to " + filter_on);
-				});
+				enable_filter(filter_button)
 			}
 		})
 	})
 
-
+	//Enable links from popup
+	var links = document.getElementsByTagName("a");
+    for (var i = 0; i < links.length; i++) {
+        (function () {
+            var ln = links[i];
+            var location = ln.href;
+            ln.onclick = function () {
+                chrome.tabs.create({active: true, url: location});
+            };
+        })();
+    }
 }
   	
-function setButtonOn(button) { //Send a message to content.js to activate button, then set styling and HTML when button is turned on
-	button.innerHTML = "Deactivate"
-	button.classList.replace("button_off", "button_on")
+function enable_filter(button) { //Send a message to content.js to activate button, then set styling and HTML when button is turned on
+	//Temporarily disable button until response is  received
+	button.disabled = true
+	//Send request to content.js to enable filter
+	chrome.tabs.query({active: true, currentWindow: true}, function(tabs){
+		chrome.tabs.sendMessage(tabs[0].id, {"action": "enable_filter"})
+		.then(response => {
+			//Check if request was successful
+			if (response.successful === true) { //Success Case
+				//If request went through fine change button text and styling
+				button.innerHTML = "Deactivate"
+				button.classList.replace("button_off", "button_on")
+				//Set the filter to on (in memory and in local storage)
+				is_filter_on = true
+				chrome.storage.local.set({ is_filter_on: is_filter_on })
+			} else { //Failure Case
+				console.log("Warning: failed to activate filter!")
+			}
+		})
+		.catch(error => {
+			console.error(error)
+		})
+		.finally(() => {
+			button.disabled = false
+		})
+	})
 }
 
-function setButtonOff(button) {
-	button.innerHTML = "Activate"
-	button.classList.replace("button_on", "button_off")
+function disable_filter(button) {
+	//Temporarily disable button until response is received
+	button.disabled = true
+	//Send request to content.js to enable filter
+	chrome.tabs.query({active: true, currentWindow: true}, function(tabs){
+		chrome.tabs.sendMessage(tabs[0].id, {"action": "disable_filter"})
+		.then(response => {
+			//Check if request was successful
+			if (response.successful === true) { //Success Case
+				// If request went through fine change button text and styling
+				button.innerHTML = "Activate"
+				button.classList.replace("button_on", "button_off")
+				//Set the filter to off (in memory and in local storage)
+				is_filter_on = false
+				chrome.storage.local.set({ is_filter_on: is_filter_on })
+			} else { //Failure Case
+				console.log("Warning: failed to activate filter!")
+			}
+		})
+		.catch(error => {
+			console.error(error)
+		})
+		.finally(() => {
+			button.disabled = false
+		})
+	})
+}
+
+function setButtonOn(button) { //Visually changes the button to reflect that the filter is on
+	button.innerHTML = "Deactivate"
+	button.classList.replace("button_off", "button_on")
 }
